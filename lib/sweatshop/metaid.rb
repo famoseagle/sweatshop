255f6d504884af5396f40974574fb5fa44eb7f35
Mike Stangel <mike@geni.com>
2011-09-27 12:27:32 -0700

    removed metaclass definition to avoid conflicting with Rails deprecation warning

--- lib/sweatshop/metaid.rb
+++ lib/sweatshop/metaid.rb
@@ -1,5 +1,4 @@
-def metaclass; class << self; self; end; end
-def meta_eval(&blk); metaclass.instance_eval(&blk); end
+def meta_eval(&blk); (class << self; self; end).instance_eval(&blk); end
 def meta_def(name, &blk)
   meta_eval { define_method name, &blk }
 end
